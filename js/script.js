f387c27448c7f3b04db06cfb4429fd2d1a1b769d
ibag <bahniukihor@gmail.com>
2023-02-10 16:06:00 +0200

    0.0.2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-import {race} from './stats.mjs';
+import race from './stats.mjs';
+
+const popupInfoAboutRace = document.querySelector('#popup__race');
+
+const body = document.querySelector('body');
+
+const popupOverlay = document.querySelector('.overlay');
+
+const popupInfoHealthPoints = document.querySelector('#HP__value');
+
+const popupInfoArmourPoints = document.querySelector('#AP__value');
+
+const popupInfoManaPoints = document.querySelector('#MP__value');
+
+const closePopupInfoAboutRace = document.querySelector('#close__race__popup');
+
+const popupInfoContentTitle = document.querySelector('.content__title');
 
 const raceHuman = document.querySelector('#race__human');
 
 const raceDwarf = document.querySelector('#race__dwarf');
 
 const raceElf = document.querySelector('#race__elf');
+
+const infoAboutRaces = document.querySelectorAll('.option__info');
+
+const screenClass = document.querySelector('.screen__class');
+
+closePopupInfoAboutRace.addEventListener('click', closePopup);
+
+popupOverlay.addEventListener('click', closePopup);
 
 const player = {
     race: '',
@@ -18,18 +42,67 @@ const player = {
 
 raceHuman.addEventListener('click', () =>{
     player.race = raceHuman.innerHTML;
-    player.stats.healthPoints = race.human.healthPoints
-    console.log(`Players race is ${player.race}. HP is ${player.stats.healthPoints}`);
+    player.stats.healthPoints = race.human.healthPoints;
+    player.stats.armourPoints = race.human.armourPoints;
+    player.stats.manaPoints = race.human.manaPoints;
+    console.log(`Player's race is ${player.race}. HP is ${player.stats.healthPoints}. AP is ${player.stats.armourPoints}. MP is ${player.stats.manaPoints}`);
+    raceHuman.closest('.screen').classList.add('hide');
+    screenClass.classList.remove('hide');
+    chooseYourClass()
 })
 
 raceDwarf.addEventListener('click', () =>{
-    console.log('Player choose Dwarf');
     player.race = raceDwarf.innerHTML;
-    console.log(player.race);
+    player.stats.healthPoints = race.dwarf.healthPoints;
+    player.stats.armourPoints = race.dwarf.armourPoints;
+    player.stats.manaPoints = race.dwarf.manaPoints;
+    console.log(`Player's race is ${player.race}. HP is ${player.stats.healthPoints}. AP is ${player.stats.armourPoints}. MP is ${player.stats.manaPoints}`);
+    raceDwarf.closest('.screen').classList.add('hide');
+    screenClass.classList.remove('hide');
+    chooseYourClass()
 })
 
 raceElf.addEventListener('click', () =>{
-    console.log('Player choose Elf');
     player.race = raceElf.innerHTML;
-    console.log(player.race);
+    player.stats.healthPoints = race.elf.healthPoints;
+    player.stats.armourPoints = race.elf.armourPoints;
+    player.stats.manaPoints = race.elf.manaPoints;
+    console.log(`Player's race is ${player.race}. HP is ${player.stats.healthPoints}. AP is ${player.stats.armourPoints}. MP is ${player.stats.manaPoints}`);
+    raceElf.closest('.screen').classList.add('hide');
+    screenClass.classList.remove('hide');
+    chooseYourClass()
 })
+
+infoAboutRaces.forEach(elem =>{
+    elem.addEventListener('click', () =>{
+        popupInfoAboutRace.classList.add('show-popup');
+        body.classList.add('noscroll');
+        popupInfoContentTitle.innerHTML = elem.closest('.choose__option').querySelector('.option__btn').innerHTML;
+        if(popupInfoContentTitle.innerHTML === 'Human'){
+            popupInfoHealthPoints.innerHTML = race.human.healthPoints;
+            popupInfoArmourPoints.innerHTML = race.human.armourPoints;
+            popupInfoManaPoints.innerHTML = race.human.manaPoints;
+        }
+
+        if(popupInfoContentTitle.innerHTML === 'Dwarf'){
+            popupInfoHealthPoints.innerHTML = race.dwarf.healthPoints;
+            popupInfoArmourPoints.innerHTML = race.dwarf.armourPoints;
+            popupInfoManaPoints.innerHTML = race.dwarf.manaPoints;
+        }
+
+        if(popupInfoContentTitle.innerHTML === 'Elf'){
+            popupInfoHealthPoints.innerHTML = race.elf.healthPoints;
+            popupInfoArmourPoints.innerHTML = race.elf.armourPoints;
+            popupInfoManaPoints.innerHTML = race.elf.manaPoints;
+        }
+    })
+})
+
+function closePopup(){
+    popupInfoAboutRace.classList.remove('show-popup');
+    body.classList.remove('noscroll');
+}
+
+function chooseYourClass(){
+    const classOption = screenClass.querySelectorAll('.option__btn');
+}
--- js/stats.mjs
+++ js/stats.mjs
@@ -4,29 +4,38 @@ const race = {
     human: {
         healthPoints: 10,
         armourPoints: 0,
-        manaPoint: 0,
+        manaPoints: 0,
         class: {
-            legionier: {
-                bonusHealthPoints: 5,
-                bonusArmourPoints: 5,
-                bonusManaPoints: 0
+            legionnaire: {
+                className: 'Legionnaire of the First imperial legion',
+                classStats: {
+                    bonusHealthPoints: 5,
+                    bonusArmourPoints: 5,
+                    bonusManaPoints: 0
+                }
             },
             scout: {
-                bonusHealthPoints: 7,
-                bonusArmourPoints: 3,
-                bonusManaPoints: 0
+                className: 'Scout of the Fifth light brigade',
+                classStats: {
+                    bonusHealthPoints: 7,
+                    bonusArmourPoints: 3,
+                    bonusManaPoints: 0
+                }
             },
             agent: {
-                bonusHealthPoints: 5,
-                bonusArmourPoints: 2,
-                bonusManaPoints: 3
+                className: 'Agent of the Imperial Intelligent',
+                classStats: {
+                    bonusHealthPoints: 5,
+                    bonusArmourPoints: 2,
+                    bonusManaPoints: 3
+                }
             }
         }
     },
     dwarf: {
         healthPoints: 7,
         armourPoints: 3,
-        manaPoint: 0,
+        manaPoints: 0,
         class: {
             guardsman: {
                 bonusHealthPoints: 5,
@@ -48,7 +57,7 @@ const race = {
     elf: {
         healthPoints: 7,
         armourPoints: 0,
-        manaPoint: 3,
+        manaPoints: 3,
         class: {
             defender: {
                 bonusHealthPoints: 4,
@@ -68,3 +77,5 @@ const race = {
         }
     }
 }
+
+export default race;
